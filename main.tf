39ba46a942b8b116bc78d1424f598fae3d9a6f19
clear0111 <134456381+clear0111@users.noreply.github.com>
2026-09-03 11:51:01 +0100

    Update AMI ID for AWS instance

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
 region = "us-east-1"
 }
 resource "aws_instance" "example" {
-ami = "ami-01c265752adadcdf8"
+ami = "ami-081b0a6eac00b4f53"
 instance_type = "t3.micro"
 tags = {
 Name = "GitHubActionsEC2"
